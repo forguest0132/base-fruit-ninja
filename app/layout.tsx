542c249236c3012b363a97737605be740d042570
forguest0132 <forguest0132@gmail.com>
2026-08-21 10:42:37 +0600

    feat: hardcode developer wallet tag in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,6 @@ import '@coinbase/onchainkit/styles.css';
 export const metadata: Metadata = {
   title: 'Base Fruit Ninja',
   description: 'Play Fruit Ninja Mini App on Base Network',
-  other: {
-    // Developer Ownership & Base Rewards Attribution
-    'creator:address': '0x4ECd53055A78bdB5DAfe9ba5154e48906FBe6AEc',
-    'eth:developer:address': '0x4ECd53055A78bdB5DAfe9ba5154e48906FBe6AEc',
-    'fc:frame': 'vNext',
-    'fc:frame:image': 'https://images.lumacdn.com/cdn-cgi/image/format=auto,fit=cover,dpr=2,quality=75,width=620,height=324.76190476190476/event-covers/1b/c2d774-897b-4024-8178-081e6a17b07c',
-    'fc:frame:button:1': 'Play Fruit Ninja 🍉',
-    'fc:frame:button:1:action': 'link',
-    'fc:frame:button:1:target': 'https://base-fruit-ninja-hazel.vercel.app',
-  },
 };
 
 export const dynamic = 'force-dynamic';
@@ -28,6 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="creator:address" content="0x4ECd53055A78bdB5DAfe9ba5154e48906FBe6AEc" />
+        <meta name="eth:developer:address" content="0x4ECd53055A78bdB5DAfe9ba5154e48906FBe6AEc" />
+        <meta name="base:developer:address" content="0x4ECd53055A78bdB5DAfe9ba5154e48906FBe6AEc" />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://images.lumacdn.com/cdn-cgi/image/format=auto,fit=cover,dpr=2,quality=75,width=620,height=324.76190476190476/event-covers/1b/c2d774-897b-4024-8178-081e6a17b07c" />
+        <meta property="fc:frame:button:1" content="Play Fruit Ninja 🍉" />
+        <meta property="fc:frame:button:1:action" content="link" />
+        <meta property="fc:frame:button:1:target" content="https://base-fruit-ninja-hazel.vercel.app" />
+      </head>
       <body>
         <OnchainProviders>
           <FrameProvider>{children}</FrameProvider>
